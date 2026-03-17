48e4d9200616f673d6534aa9cdbb28e15f48f4f9
Lilia-Sirgalina <lilia_developer@hotmail.com>
2026-03-17 15:59:24 +0100

    finishing the app logic

--- src/Components/Cart/CartItem.jsx
+++ src/Components/Cart/CartItem.jsx
@@ -1,9 +1,12 @@
+import { useDispatch } from "react-redux";
 import { dishesData } from "../../data/dataDishes";
+import { removeItemFromCart } from "../../redux/cartSlice";
 
 const CartItem = ({cartItem}) => {
 
     console.log(cartItem);
     const dishes = dishesData.find((item) => item.id === cartItem.dishID)
+    const dispatch = useDispatch()
     console.log(dishes)
 
     return(
@@ -11,7 +14,10 @@ const CartItem = ({cartItem}) => {
             <p>{dishes.name}</p>
             <p>{cartItem.quantity} portion(s)</p>
             <p>Price: ${dishes.price * cartItem.quantity}</p>
-            
+
+            <span onClick={() => dispatch(removeItemFromCart({cartItemID: cartItem.id}))} >
+                <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png"/> 
+            </span>
         </div>
     )
 }
--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -9,15 +9,28 @@ export const slice = createSlice({
 
   reducers: {
     addItemToCart: (state, action) => {
+      const timeID = new Date().getTime()
+
       state.cartItems.push({
+        id: timeID,
         dishID: action.payload.dish.id,
-        quantity: action.payload.quantity
+        quantity: action.payload.quantity,
+        totalPrice: action.payload.dish.price * action.payload.quantity
       })
-    },   
+    }, 
+    
+    removeItemFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(cartItem => cartItem.id !== action.payload.cartItemID)
+    }
   },
 })
 
 
+export const getTotalPrice = state => {
+  return state.cart.cartItems.reduce((total, cartItems) => {
+    return total + cartItems.totalPrice 
+  }, 0)
+}
 export const getCartItems = state => state.cart.cartItems;
-export const { addItemToCart } = slice.actions;
+export const { addItemToCart, removeItemFromCart } = slice.actions;
 export default slice.reducer
